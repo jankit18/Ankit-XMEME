7a11936e187c87c554b428f4f0c691c0522a4f88
Ankit Jaiswal <jankit360@gmail.com>
2021-02-15 21:41:57 +0530

    Update settings.py

--- XMEME/XMEME/settings.py
+++ XMEME/XMEME/settings.py
@@ -20,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '+lfu-h+ik&w_d^y_ks^r7-jr=-(e9t38y=6#*ccytzrj$c#(x8'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
